0e20d816520b6434ee7049afad5379c1844f9e42
Barnaby <22575741+barnabwhy@users.noreply.github.com>
2023-07-13 21:36:00 +0100

    cli test include sound + compressibles

--- cli_test.ts
+++ cli_test.ts
@@ -3,11 +3,13 @@ import { VPK, VPKCopy, VPKCopyProgress, VPacker } from "./src";
 (async () => {
     let startTime = Date.now();
 
+    console.log("VPK Extraction:");
+
     const vpkName = "englishclient_mp_common.bsp.pak000_dir.vpk";
     const vpkPath = "F:\\SteamLibrary\\steamapps\\common\\Titanfall\\vpk\\" + vpkName;
     let vpk = new VPK(vpkPath);
     vpk.readTree();
-    const files = Object.keys(vpk.tree.files).filter(f => f.startsWith("sound/campaign"));
+    const files = Object.keys(vpk.tree.files).filter(f => f.startsWith("sound/campaign") || f.startsWith("resource"));
     const outPath = "./out"
 
     let copier = new VPKCopy(vpkPath, 16);
@@ -24,13 +26,18 @@ import { VPK, VPKCopy, VPKCopyProgress, VPacker } from "./src";
 
     startTime = Date.now();
 
-    const newVpkName = "englishclient_audio_campaign.bsp.pak000_dir.vpk";
+    console.log("\nVPK Packing:");
+
+    const newVpkName = "englishclient_test.bsp.pak000_dir.vpk";
     let revpk = new VPacker();
+    let i = 1;
     for (const file of files) {
-        console.log(`Adding file ${outPath}/${file}`);
+        console.log(`${i}/${files.length}\t | Adding file ${outPath}/${file}`);
         await revpk.addFile(outPath + '/' + file, file)
+        i++;
     }
 
+    console.log(`Writing VPK ${newVpkName}`);
     revpk.writeVPK(outPath + '/' + newVpkName);
 
     console.log(`Packing took: ${Date.now() - startTime}ms`)
